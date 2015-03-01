bff8b5396db2eca9e69134f0bfc0f48a56bda1c4
chrisjamhol <chrisjamhol@gmail.com>
2015-03-01 15:05:32 +0100

    added weather support

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,26 +10,31 @@
   app.locations = [
     {
       'name': 'Ruhestein',
-      'webcam': 'http://www.skilift-ruhestein.de/pics/webcam.jpg'
+      'webcam': 'http://www.skilift-ruhestein.de/pics/webcam.jpg',
+      'weatherNow': 'http://api.openweathermap.org/data/2.5/weather?q=Ruhestein,de&lang=de',
+      'weather3Days': null
     },
     {
       'name': 'Seibels Eckle',
-      'webcam': 'http://www.seibelseckle.de/seibelseckle-webcam-lift.jpg?dummy=8484744'
+      'webcam': 'http://www.seibelseckle.de/seibelseckle-webcam-lift.jpg?dummy=8484744',
+      'weatherNow': null,
+      'weather3Days': null
     },
     {
-      "name": "Vogelskopf",
-      "webcam": "http://www.vogelskopf.de/webcam/Vogelskopf_R1.jpg",
-      "weather": "http://www.seibelseckle.de/seibelseckle-webcam-huette.jpg?dummy=8484744"
+      'name': 'Vogelskopf',
+      'webcam': 'http://www.vogelskopf.de/webcam/Vogelskopf_R1.jpg',
+      'weatherNow': 'http://api.openweathermap.org/data/2.5/weather?q=Vogelskopf,de&lang=de',
+      'weather3Days': null
     },
     {
-      "name": "Zufluch",
-      "webcam": "http://www.vogelskopf.de/webcam/Zuflucht_R1.jpg",
-      "weather": "http://www.seibelseckle.de/seibelseckle-webcam-huette.jpg?dummy=8484744"
+      'name': 'Zufluch',
+      'webcam': 'http://www.vogelskopf.de/webcam/Zuflucht_R1.jp'
     },
     {
-      "name": "Kniebis",
-      "webcam": "http://www.itunds.de/nachtloipe_webcam/skicam.jpg",
-      "weather": "http://www.seibelseckle.de/seibelseckle-webcam-huette.jpg?dummy=8484744"
+      'name': 'Kniebis',
+      'webcam': 'http://www.itunds.de/nachtloipe_webcam/skicam.jpg',
+      'weatherNow': 'http://api.openweathermap.org/data/2.5/weather?q=Kniebis,de&lang=de',
+      'weather3Days': null
     }
   ];
 
